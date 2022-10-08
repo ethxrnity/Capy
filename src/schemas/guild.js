501d6d5949eec5848e6214d88aadd13f77b2e4bb
Ladislav Král <ethxrnity@gmail.com>
2022-10-09 01:32:46 +0200

    Alfa

--- src/schemas/guild.js
+++
@@ -1,8 +0,0 @@
-const { Schema, model } = require('mongoose');
-const guildSchema = new Schema({
-    guildId: String,
-    guildName: String,
-    guildIcon: { type: String, required: true },
-});
-
-module.exports = model("Guild", guildSchema, "guilds");
